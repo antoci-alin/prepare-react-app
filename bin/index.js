96c982a6b256ff029f4c5704f86839211b16ce58
Alin Georgian Antoci <brinza.alin.georgian@ro.ibm.com>
2019-12-31 04:21:17 +0200

    almost ready

--- bin/index.js
+++ bin/index.js
@@ -3,8 +3,8 @@
 const colors = require("colors");
 const ora = require("ora");
 
-const dep = require("../lib/getDepedencies");
-const { checkFiles, removeFiles } = require("../lib/fileActions");
+const { getDepVer, getReactRouterDOM } = require("../lib/getDepedencies");
+const { removeFiles, manageFolders, createStyleFiles } = require("../lib/fileActions");
 
 const spinner = new ora({
 	color: "blue",
@@ -13,20 +13,26 @@ const spinner = new ora({
 });
 
 (async () => {
-	//  Get list of dependences
 	console.log(`\n። Checking if ${colors.bgBlue("create-react-app")} is installed`);
 	spinner.start();
-	let depList = await dep.getDepVer();
+	let depList = await getDepVer();
 
 	if (depList.length > 0) {
 		spinner.succeed(` ${colors.bgBlue("create-react-app")} installed`);
-	} else {
-		spinner.succeed(` ${colors.bgBlue("create-react-app")} installed`);
 	}
+
 	spinner.stop();
 
-	console.log(`\n። Checking your files`);
-	await checkFiles();
-	console.log(`\n። Removing files`);
+	console.log(`\n። Installing ${colors.bgBlue("react-router-dom & node-sass")}`);
+	spinner.start();
+	await getReactRouterDOM();
+	spinner.stop();
+	spinner.succeed(` ${colors.bgBlue("react-router-dom")} installed`);
+
+	console.log(`\n። Preparing your project`);
 	await removeFiles();
+	await manageFolders();
+	await createStyleFiles();
+
+	console.log(`\n። All set!\n\n You can now start your server with ${colors.bgRed("yarn start")}`);
 })();
